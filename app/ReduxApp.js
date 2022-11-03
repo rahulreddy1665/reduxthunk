4cd28ef2081efda2e2143f16414b31f68a5513cf
Rahulweezy <rahul.reddy@agileidc.com>
2022-11-03 18:38:32 +0530

    React-native contacts fetch

--- app/ReduxApp.js
+++ app/ReduxApp.js
@@ -1,20 +1,60 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet, Button, ActivityIndicator} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  ActivityIndicator,
+  ScrollView,
+} from 'react-native';
 import * as actions from '../app/actions/actions';
 import {connect} from 'react-redux';
+import {PermissionsAndroid} from 'react-native';
+import Contacts from 'react-native-contacts';
+import {useState} from 'react';
 
 const ReduxAsyncApp = props => {
+  const [Contatcs, setContacts] = useState([]);
+  const LoadContatcs = () => {
+    PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_CONTACTS, {
+      title: 'Contacts',
+      message: 'This app would like to view your contacts.',
+      buttonPositive: 'Please accept bare mortal',
+    }).then(
+      Contacts.getAll()
+        .then(contacts => {
+          // work with contacts
+          console.log('Totoal Contatcs' + JSON.stringify(contacts));
+          setContacts(contacts);
+        })
+        .catch(e => {
+          console.log(e);
+        }),
+    );
+  };
   return (
-    <View style={styles.container}>
+    <ScrollView>
       {props.isLoading ? (
         <ActivityIndicator />
       ) : (
         <View>
           <Text style={{fontSize: 24, textAlign: 'center'}}>{props.quote}</Text>
           <Button title="Load Quote" onPress={() => props.loadQuote()} />
+          <Button
+            title="Load contacts"
+            style={{marginTop: 20}}
+            onPress={LoadContatcs}></Button>
+          {Contatcs.map(item => (
+            <View style={{justifyContent: 'center'}}>
+              <Text style={{fontSize: 18}}>
+                {' '}
+                {`${item.givenName} ${item.familyName}`}{' '}
+              </Text>
+            </View>
+          ))}
         </View>
       )}
-    </View>
+    </ScrollView>
   );
 };
 
